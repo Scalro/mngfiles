113b3815f1e9f16adc9175d3a20252d24736ea55
Scalro <rotichcaleb200@gmail.com>
2024-07-24 16:33:51 +0300

    u7\pdate

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,35 +9,37 @@ import {
 
 function Footer() {
   return (
-    <div className="foot">
-      <h3 className="log">SpotFile</h3>
-      <span className="tact">
-        <h5>
-          <MdLocalPhone /> +254720964498
-        </h5>
-        <h5>
-          <MdMail /> info@spotfile.com
-        </h5>
-        <span style={{ padding: "5px" }}>|</span>
-        <h5>Privacy Policy</h5>
-        <h5>Code of Ethics</h5>
-        <span style={{ padding: "5px" }}>|</span>
-        <span className="social">
-          <a href="#!">
-            <FaXTwitter />
-          </a>
-          <a href="!#">
-            <FaFacebookF />
-          </a>
-          <a href="!#">
-            <FaWhatsapp />
-          </a>
-          <a href="!#">
-            <FaLinkedin />
-          </a>
+    <div className="main">
+      <div className="foot">
+        <h3 className="log">SpotFile</h3>
+        <span className="tact">
+          <h5>
+            <MdLocalPhone /> +254720964498
+          </h5>
+          <h5>
+            <MdMail /> info@spotfile.com
+          </h5>
+          <span style={{ padding: "5px" }}>|</span>
+          <h5>Privacy Policy</h5>
+          <h5>Code of Ethics</h5>
+          <span style={{ padding: "5px" }}>|</span>
+          <span className="social">
+            <a href="#!">
+              <FaXTwitter />
+            </a>
+            <a href="!#">
+              <FaFacebookF />
+            </a>
+            <a href="!#">
+              <FaWhatsapp />
+            </a>
+            <a href="!#">
+              <FaLinkedin />
+            </a>
+          </span>
         </span>
-      </span>
-      <h6>© Copyright Chemwotei 2024. All Rights Reserved</h6>
+        <h6>© Copyright Chemwotei 2024. All Rights Reserved</h6>
+      </div>
     </div>
   );
 }
--- src/components/Services/Filem.jsx
+++ src/components/Services/Filem.jsx
@@ -30,16 +30,16 @@ function Filem() {
           <div className="cn">
             <h1>Scanning</h1>
             <p>
-              Metrofile has captured over 60 million lines of data and stored
+              SpotFile has captured over 60 million lines of data and stored
               over 309 million images, making them easily accessible for
               clients. This service allows businesses to digitize their
               documents, reducing the need for physical storage and improving
               overall efficiency.The company currently stores over 8.5 million
               boxes, ensuring the safe and secure storage of your documents. The
-              conversion process can be carried out by Metrofile through either
+              conversion process can be carried out by SpotFile through either
               an outsourced arrangement whereby the final product will be held
-              and managed on Metrofile’s systems, an insourced arrangement
-              whereby Metrofile manages the infrastructure and staffing on
+              and managed on SpotFile’s systems, an insourced arrangement
+              whereby SpotFile manages the infrastructure and staffing on
               customer premises or a customised combination of both.
             </p>
             <div className="ctn">
@@ -79,52 +79,66 @@ function Filem() {
               experience ensures effective management of documents and records
               throughout every part of the information life cycle.
             </p>
-            <h3>Vault Storage</h3>
-            <p>
-              Our secure, climate-controlled vaults provide the utmost
-              protection against moisture, dust and contaminants and are weather
-              and fire-resistant with gas suppression. These facilities are
-              constantly monitored by CCTV, are strictly access controlled and
-              are protected by perimeter fencing.
-            </p>
-            <h3>Records Storage</h3>
-            <p>
-              Records are safe in our purpose-built, world-class warehouses
-              which meet industry standards to protect against elements. Our
-              sophisticated labelling, indexing, search, retrieval, tracking and
-              re-filing system always guarantees quick and easy access to your
-              information.
-            </p>
-            <h3>Active Filing</h3>
-            <p>
-              Records can be deep stored for extended periods of time or, for
-              customers who need quick and easy access to their files on an
-              ongoing basis, we offer cost-effective active filing solutions.
-            </p>
+            <div className="ctn">
+              <div className="crd">
+                <h3>Vault Storage</h3>
+                <p>
+                  Our secure, climate-controlled vaults provide the utmost
+                  protection against moisture, dust and contaminants and are
+                  weather and fire-resistant with gas suppression. These
+                  facilities are constantly monitored by CCTV, are strictly
+                  access controlled and are protected by perimeter fencing.
+                </p>
+              </div>
+              <div className="crd">
+                <h3>Records Storage</h3>
+                <p>
+                  Records are safe in our purpose-built, world-class warehouses
+                  which meet industry standards to protect against elements. Our
+                  sophisticated labelling, indexing, search, retrieval, tracking
+                  and re-filing system always guarantees quick and easy access
+                  to your information.
+                </p>
+              </div>
+              <div className="crd">
+                <h3>Active Filing</h3>
+                <p>
+                  Records can be deep stored for extended periods of time or,
+                  for customers who need quick and easy access to their files on
+                  an ongoing basis, we offer cost-effective active filing
+                  solutions.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
         <div className={toggle === 3 ? "tbs" : "tabs"}>
           <div className="cn">
             <h1>Filing and Storage Products</h1>
-            <p>
-              Metrofile offers a range of filing and storage products to help
-              businesses keep their documents organized and easily accessible.
-              From POPIA compliant files for medical businesses, to files and
-              accessories, archive and off-site storage boxes, filing cabinets,
-              bulk filers and shelving products, we are a leading provider of
-              filing solutions for every business regardless of size or nature.
-              Our comprehensive range of products can be customised to suit your
-              every requirement. We have expanded our product range to include
-              boxes, labelling and packaging material required for moving and
-              packing for both business and commercial use.
-            </p>
+            <div className="ig">
+              <img src="/Images/filing.png" alt="filing" />
+
+              <p>
+                SpotFile offers a range of filing and storage products to help
+                businesses keep their documents organized and easily accessible.
+                From POPIA compliant files for medical businesses, to files and
+                accessories, archive and off-site storage boxes, filing
+                cabinets, bulk filers and shelving products, we are a leading
+                provider of filing solutions for every business regardless of
+                size or nature. Our comprehensive range of products can be
+                customised to suit your every requirement. We have expanded our
+                product range to include boxes, labelling and packaging material
+                required for moving and packing for both business and commercial
+                use.
+              </p>
+            </div>
           </div>
         </div>
         <div className={toggle === 4 ? "tbs" : "tabs"}>
           <div className="cn">
             <h1>Confidential Destruction</h1>
             <p>
-              Metrofile offers a range of filing and storage products to help
+              SpotFile offers a range of filing and storage products to help
               businesses keep their documents organized and easily accessible.
               Your records can be a goldmine for identity thieves and you may be
               inadvertently helping them. Protect your identity, save yourself
